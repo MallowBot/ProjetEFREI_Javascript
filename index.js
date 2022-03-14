b915805c6d79d6030be296c46ef3f0e2cd100b1a
Grimyolf <95622304+Grimyolf@users.noreply.github.com>
2022-03-14 18:45:38 +0100

    Changement ID turnPlayer

--- index.js
+++ index.js
@@ -1,36 +1,102 @@
 const tour = document.querySelector("h3");
-let gameActive = true;
-let redToggled = false;
-let blackToggled = false;
-let playerActiv = "Rouge";
+// Messages
 
-// 0:vide /1:Pion red /2:Pion black /3:king red /4:king black
-let empty = 0;
+
+// Valeurs:
+let gameActive = true;
+let playerActiv = "Rouge";
+let playerTurn = true;
+let playerPion;
+let scoreRed = 12;
+let scoreBlack = 12;
+
+
+// 0:vide /1:Pion red  /2:Pion black /3:king red /4:king black
+let vide = 0;
 let pionRed = 1;
 let pionBlack = 2;
 let kingRed = 3;
 let kingBlack = 4;
-let table = [
-  "2","0","2","0","2","0","2","0",
-  "0","2","0","2","0","2","0","2",
-  "2","0","2","0","2","0","2","0",
-  "0","0","0","0","0","0","0","0",
-  "0","0","0","0","0","0","0","0",
-  "0","1","0","1","0","1","0","1",
-  "1","0","1","0","1","0","1","0",
-  "0","1","0","1","0","1","0","1",
+
+let playTable = [
+  23,null,22,null,21,null,20,null,
+  null,19,null,18,null,17,null,16,
+  15,null,14,null,13,null,12,null,
+  null,null,null,null,null,null,null,null,
+  null,null,null,null,null,null,null,null,
+  null,11,null,10,null,9,null,8,
+  7,null,6,null,5,null,4,null,
+  null,3,null,2,null,1,null,0,
 ]
 
-// Messages
+let originTable = [
+  2,0,2,0,2,0,2,0,
+  0,2,0,2,0,2,0,2,
+  2,0,2,0,2,0,2,0,
+  0,0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,0,
+  0,1,0,1,0,1,0,1,
+  1,0,1,0,1,0,1,0,
+  0,1,0,1,0,1,0,1,
+]
+
 const gagne = () => `Le joueur ${playerActiv} a gagné`
 const tourPlayer = () => `C'est au tour du joueur ${playerActiv}`
-
 tour.innerHTML = tourPlayer()
 
-document.querySelectorAll(".case-w").forEach(cell => cell.addEventListener("click",clicCell))
 
-function clicCell() {
-  const indexCell = parseInt(this.dataset.index)
-  console.log(indexCell)
-  document.getElementById(`${indexCell}`).innerHTML = `<img src="./assets/Pion_Red.png" height="55" width="55">`
+function pionEventListener() {
+  if (playerTurn){
+    document.querySelectorAll(".Red-pion").forEach(cell => cell.addEventListener("click",clicCellR))
+    clicCellR();
+  }
+  else {
+    document.querySelectorAll(".Black-pion").forEach(cell => cell.addEventListener("click",clicCellB))
+    clicCellB();
+  }
 }
+
+
+function clicCellR() {
+  const indexCellR = parseInt(this.dataset.index)
+  document.getElementById(`${indexCellR}`).innerHTML = `<img src="./assets/Pion_Red.png" height="00" width="00">`
+}
+
+function clicCellB() {
+  const indexCellB = parseInt(this.dataset.index)
+  document.getElementById(`${indexCellB}`).innerHTML = `<img src="./assets/Pion_Black.png" height="55" width="55">`
+}
+
+// document.querySelectorAll(".case-w").forEach(cell => cell.addEventListener("click",hideDot))
+// function hideDot() {
+//   const indexCellHideB = parseInt(this.dataset.index) 
+//   document.getElementById(`${indexCellHideB}`).innerHTML = ""
+// }
+
+
+
+
+
+
+
+
+// Restart
+const indice =1
+let clicButton = document.querySelector("#restart");
+
+clicButton.onclick = function restart(){
+  for (let j=0; j<originTable.length; j++){
+    originTable.forEach(function (value, index){
+     if (value==1){
+      document.getElementById(index).innerHTML = `<img src="./assets/Pion_Red.png" height="55" width="55">`
+     }
+     if (value==2){
+      document.getElementById(index).innerHTML = `<img src="./assets/Pion_Black.png" height="55" width="55">`
+     }
+     if (value==0){
+      document.getElementById(index).innerHTML = ``
+     }
+    });
+  }
+}
+
